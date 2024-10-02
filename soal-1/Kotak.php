973e3067ce84450eee7f44950692bbe0d46aceff
gey <gey@gmail.com>
2024-10-02 22:33:15 +0800

    tugas selesai

--- soal-1/Kotak.php
+++ soal-1/Kotak.php
@@ -2,28 +2,37 @@
 class PersegiPanjang
 {
     // deklarasikan panjang dan lebar
-
+    public $panjang;
+    public $lebar;
 
     // deklarasikan metode konstruktor
-
-
-    // deklarasikan metode mutator dan asesor
-
-
-
-    public function hitungLuas()
-    {
-        // lengkapi kode untuk menghitung luas persegi panjang
-
+    public function __construct($panjang,$lebar) {
+        $this->panjang = $panjang;
+        $this->lebar = $lebar;
     }
 
-    public function hitungKeliling()
-    {
-        // lengkapi dengan kode untuk menghitung keliling persegi panjang
+    // deklarasikan metode mutator dan asesor
+    public function __get($atribut) {
+        return $this->$atribut;
+    }
 
+    public function __set($atribut, $nilai) {
+        $this->$atribut = $nilai;
+    }
+
+
+    public function hitungLuas() {
+        // lengkapi kode untuk menghitung luas persegi panjang
+        return $this->panjang * $this->lebar;
+    }
+
+    public function hitungKeliling() {
+        // lengkapi dengan kode untuk menghitung keliling persegi panjang
+        return 2 * ($this->panjang + $this->lebar);
     }
 }
 
 $contoh = new PersegiPanjang(12, 20);
 echo "Luas Persegi Panjang: " . $contoh->hitungLuas() . "<br>";
 echo "Keliling Persegi Panjang: " . $contoh->hitungKeliling() . "<br>";
+?>
--- soal-2/Waktu.php
+++ soal-2/Waktu.php
@@ -2,8 +2,45 @@
 class Waktu
 {
     // deklarasikan atribut sesuai ditentukan buku
-
+    public $jam;
+    public $menit;
+    public $detik;
     // deklarasikan metode sesuai yang diminta buku
+    public function __construct($jam,$menit,$detik) {
+        $this->jam = $jam;
+        $this->menit = $menit;
+        $this->detik = $detik;
+    }
+
+    public function getjam() {
+        return $this->jam;
+    }
+    public function getmenit() {
+        return $this->menit;
+    }
+    public function getdetik() {
+        return $this->detik;
+    }
+    public function setjam() {
+        $this->jam = $Jam;
+    }
+    public function setmenit() {
+        $this->menit = $menit;
+    }
+    public function setdetik() {
+        $this->detik = $detik;
+    }
+    public function assignwaktu($jam,$menit,$detik) {
+        $this->setjam($jam);
+        $this->setmenit($menit);
+        $this->setdetik($detik);
+    }
+    public function __get($atribut) {
+        return $this->$atribut;
+    }
+    public function __set($atribut, $Nilai) {
+        $this->$atribut = $Nilai;
+    }
 }
 
 $contoh = new Waktu(15,34,50);
